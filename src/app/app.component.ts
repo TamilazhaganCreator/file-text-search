8c531826acadac4b79c00721469f8d37faa048ff
Tamil <tamilazhagan.m@fundsindia.com>
2020-08-28 10:20:23 +0530

    file text search added with upload file functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'file-text-search';
+
+  fileContent: string = "";
+  searchText: string;
+  noOfOccurrence: number = 0;
+  uploadedFileName: string;
+  @ViewChild("inputUpload", { static: false }) private inputUpload: ElementRef;
+
+  constructor() {
+
+  }
+
+  getTextFileDetails(event) {
+    let file = event.target.files[0];
+    this.uploadedFileName = file.name;
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.fileContent = fileReader.result.toString();
+    }
+    fileReader.readAsText(file);
+  }
+
+  upload() {
+    this.inputUpload.nativeElement.click();
+  }
+
+  searchTextFunc() {
+    this.fileContent = this.fileContent.replace(/(<mark>|<\/mark>)/igm, "");
+    if (this.searchText) {
+      let searchTextRegex = this.searchText.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+      const regexExp = new RegExp(searchTextRegex, 'gi');
+      this.noOfOccurrence = (this.fileContent.match(regexExp) || []).length;
+      if (this.noOfOccurrence > 0)
+        this.fileContent = this.fileContent.replace(regexExp, (match) => `<mark>${match}</mark>`);
+    } else
+      this.noOfOccurrence = 0;
+  }
 }
